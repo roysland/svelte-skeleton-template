0575370e4897502c1b00edea703abbbc529a168b
Øyvind Roysland <oyvind.roysland@kabal.com>
2023-02-21 14:30:18 +0100

    first

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+import '@skeletonlabs/skeleton/styles/all.css';
+import './app.postcss';
+import App from "./App.svelte";
+
+const app = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
